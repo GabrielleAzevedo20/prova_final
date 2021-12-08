5c52e84b0547e56277a1e8a4bd48c867a80f343e
GabrielleAzevedo20 <gabypedroso20@gmail.com>
2021-12-07 21:44:25 -0300

    pronta

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { Text, Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import logoPng from '../../assets/logopng.png'
 import { Container } from './styles';
 
@@ -9,6 +11,8 @@ export function Header(){
   return (
     <Container>
       <Image source={logoPng} />
+      <MaterialCommunityIcons name="circle-half-full" size={24} color="blue" />
+      <MaterialIcons name="location-off" size={24} color="blue" /> 
         <Text>Identifique-se no gov.br com</Text>
     </Container>
   );
